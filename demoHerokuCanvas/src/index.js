25c3a38b4b1b35cdebbeeb7b660bdcc09b8bdf3f
Paul Roth <paulroth3d@gmail.com>
2018-08-18 13:12:46 -0400

    IMPORT: setup eslint / sass

--- demoHerokuCanvas/src/index.js
+++ demoHerokuCanvas/src/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
 
 //-- defined in the heroku configuration (if running on heroku)
 //-- or defined in the .bashrc file (if running locally)
-const CONSUMER_KEY = process.env.CONSUMER_KEY;
-const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
+const { CONSUMER_KEY, CONSUMER_SECRET } = process.env;
 
 console.log(`consumer key[${CONSUMER_KEY}]`);
 console.log(`consumer secret[${CONSUMER_SECRET}]`);
@@ -15,4 +15,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
